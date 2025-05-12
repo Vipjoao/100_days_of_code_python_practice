947b8dc2cf8dff419dabc471bdd6f91ba98e5b5b
vipjoao <tuwinkjf@gmail.com>
2025-05-12 17:17:41 -0300

    Fix variable names to unmatch a default variable

--- Final Projects/day_3.py
+++ Final Projects/day_3.py
@@ -2,12 +2,12 @@
 name = input("What is your name?\n")
 print(f"Welcome! {name}, to the Treasure Iland.\nYour mission is to find the lost treasure.")
 
-dir = input("You're in the top of a hill, do you want to go left or right?\n")
-if dir == "left":
-    dir = input("You fell down into a cave, but somehow you survived\ndo you want to swim the lake or go to the dark path?\n")
-    if dir == "swim":
-        dir = input("You find a river of aligators\nbut somehow you managed to pass through them\nand arrives in a three way split.\nDo you go, left, right or middle?\n")
-        if dir == "left" or dir == "right":
+direction = input("You're in the top of a hill, do you want to go left or right?\n")
+if direction == "left":
+    direction = input("You fell down into a cave, but somehow you survived\ndo you want to swim the lake or go to the dark path?\n")
+    if direction == "swim":
+        direction = input("You find a river of aligators\nbut somehow you managed to pass through them\nand arrives in a three way split.\nDo you go, left, right or middle?\n")
+        if direction == "left" or direction == "right":
             print(f"You were so close! {name}.\nBut you fell on a pit of deadly black mambas and died.")
         else:
             print(f"Congrats! {name}. You have found the hidden treasure!")
