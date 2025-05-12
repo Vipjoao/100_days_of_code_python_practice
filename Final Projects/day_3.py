a5b3f97c5783befed60106e0c1c94bec62b125a8
vipjoao <tuwinkjf@gmail.com>
2025-05-12 17:16:30 -0300

    Finish day 3 final challenge

---
+++ Final Projects/day_3.py
@@ -0,0 +1,17 @@
+
+name = input("What is your name?\n")
+print(f"Welcome! {name}, to the Treasure Iland.\nYour mission is to find the lost treasure.")
+
+dir = input("You're in the top of a hill, do you want to go left or right?\n")
+if dir == "left":
+    dir = input("You fell down into a cave, but somehow you survived\ndo you want to swim the lake or go to the dark path?\n")
+    if dir == "swim":
+        dir = input("You find a river of aligators\nbut somehow you managed to pass through them\nand arrives in a three way split.\nDo you go, left, right or middle?\n")
+        if dir == "left" or dir == "right":
+            print(f"You were so close! {name}.\nBut you fell on a pit of deadly black mambas and died.")
+        else:
+            print(f"Congrats! {name}. You have found the hidden treasure!")
+    else:
+        print(f"{name} decides do try the dark path.\nBut you run into a big troll that swings his giant mace at you and you die.")
+else:
+    print("You slip and fall down the montain into a painful death.")
