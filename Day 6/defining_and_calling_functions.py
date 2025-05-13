44ba048c69a1108f9990e4835d0161dba64c48cb
vipjoao <tuwinkjf@gmail.com>
2025-05-13 11:56:02 -0300

    Finished day 6 lessons

---
+++ Day 6/defining_and_calling_functions.py
@@ -0,0 +1,12 @@
+
+def my_function():
+    print("Hello!")
+    print("Bye!\n")
+
+my_function()
+
+def my_function2(first_input, last_input):
+    print(first_input)
+    print(last_input)
+
+my_function2("Hello!", "Goodbye!")
