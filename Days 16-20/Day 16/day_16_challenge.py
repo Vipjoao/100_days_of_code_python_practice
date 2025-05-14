fee94f991575335ffc1f64c565922fba8ab14dca
vipjoao <tuwinkjf@gmail.com>
2025-05-14 15:54:56 -0300

    Fix day 16 challenge classes

--- Days 16-20/Day 16/day_16_challenge.py
+++ Days 16-20/Day 16/day_16_challenge.py
@@ -3,24 +3,23 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 import art
 
-
+menu = Menu()
+coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
 
 def main():
     machine_on = True
     print(art.logo)
     while machine_on:
-        menu = Menu()
-        cm = CoffeeMaker()
-        money_machine = MoneyMachine()
         user_order = str(input(f"What would you like to order? 'Latte', 'Cappuccino' or 'Espresso'?\n")).lower()
         if user_order in menu.get_items():
-            if cm.is_resource_sufficient(menu.find_drink(user_order)):
+            if coffee_machine.is_resource_sufficient(menu.find_drink(user_order)):
                 if money_machine.make_payment(menu.find_drink(user_order).cost):
-                    cm.make_coffee(menu.find_drink(user_order))
+                    coffee_machine.make_coffee(menu.find_drink(user_order))
                 else:
                     print(f"Sorry, you don't have enough money! Here's your money back!")
         elif user_order == "report":
-            cm.report()
+            coffee_machine.report()
             money_machine.report()
         elif user_order == "off":
             machine_on = False
