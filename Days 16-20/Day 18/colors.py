3ae1dba74ecf0bd962087c63a1aa1f65e7ae6181
vipjoao <tuwinkjf@gmail.com>
2025-05-15 10:51:11 -0300

    Remove white color from colors file

--- Days 16-20/Day 18/colors.py
+++ Days 16-20/Day 18/colors.py
@@ -1,9 +1,9 @@
 # A set of colors that are compatible with the 'turtle' package
 tur_colors = [
     "red", "green", "blue", "yellow", "orange",
-    "purple", "pink", "brown", "black", "white",
+    "purple", "pink", "brown", "black", "tan",
     "gray", "cyan", "magenta", "violet", "indigo",
     "turquoise", "gold", "silver", "maroon", "lime",
     "olive", "navy", "teal", "aqua", "coral",
-    "salmon", "khaki", "orchid", "plum", "tan"
+    "salmon", "khaki", "orchid", "plum",
 ]
