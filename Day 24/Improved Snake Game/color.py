2d839346f3df861e61beaa371bcb3c638e6476a8
vipjoao <tuwinkjf@gmail.com>
2025-05-16 15:31:43 -0300

    Commit unfinished day 24

---
+++ Day 24/Improved Snake Game/color.py
@@ -0,0 +1,8 @@
+color_list = [
+    "red", "green", "blue", "yellow", "orange",
+    "purple", "pink", "black", "tan",
+    "cyan", "magenta", "violet", "indigo",
+    "turquoise", "gold", "silver", "lime",
+    "olive", "navy", "teal", "aqua", "coral",
+    "salmon", "khaki", "orchid", "plum",
+]
---
+++ Day 24/Improved Snake Game/score.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.teleport(0, 220)
+        self.penup()
+        self.color("white")
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f"Current score: {self.score} / High Score {self.high_score}", align="center", font=("Arial", 12, "bold"))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
