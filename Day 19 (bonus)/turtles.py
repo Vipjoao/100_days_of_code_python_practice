1882411a8d8f6d8257bf24ecec8712c9978f253c
vipjoao <tuwinkjf@gmail.com>
2025-05-16 11:24:02 -0300

    Add turtles class file with improved and new methods

---
+++ Day 19 (bonus)/turtles.py
@@ -0,0 +1,55 @@
+from colors import colors
+from turtle import Turtle
+import random
+
+STARTING_X_AXIS = 320
+STARTING_Y_AXIS = 75
+TURTLE_OFFSET = 30
+
+class Turtles(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.turtles_list = []
+        self.x_axis = STARTING_X_AXIS
+        self.y_axis = STARTING_Y_AXIS
+        self.create_track()
+        self.create_turtles()
+
+    def create_turtles(self):
+        for i in range(len(colors)):
+            new_turtle = Turtle(shape="turtle")
+            new_turtle.color(colors[i])
+            new_turtle.penup()
+            new_turtle.goto(-self.x_axis, self.y_axis)
+            self.y_axis -= TURTLE_OFFSET
+            self.turtles_list.append(new_turtle)
+
+    def create_track(self):
+        new_turtle = Turtle(shape="turtle")
+        new_turtle.pencolor("black")
+        new_turtle.penup()
+        new_turtle.goto(-(self.x_axis + TURTLE_OFFSET),self.y_axis + TURTLE_OFFSET)
+        new_turtle.pendown()
+        new_turtle.goto(self.x_axis + TURTLE_OFFSET,self.y_axis + TURTLE_OFFSET)
+        new_turtle.penup()
+        new_turtle.goto(self.x_axis + TURTLE_OFFSET, -(self.y_axis + TURTLE_OFFSET))
+        new_turtle.pendown()
+        new_turtle.goto(-(self.x_axis + TURTLE_OFFSET + 20), -(self.y_axis + TURTLE_OFFSET))
+
+    def turtles_run(self):
+        for tur in self.turtles_list:
+            random_distance = random.randint(1, 10)
+            tur.forward(random_distance)
+
+    def check_win(self, user_bet):
+        for tur in self.turtles_list:
+            if tur.xcor() >= 340:
+                winning_color = tur.pencolor()
+                if winning_color == user_bet:
+                    print(f"You won! The {winning_color} turtle is best!")
+                    return False
+                else:
+                    print(f"You lost! The {winning_color} won!")
+                    return False
+        return True
